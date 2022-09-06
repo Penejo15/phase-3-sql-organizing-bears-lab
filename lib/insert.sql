196b4a05183793554a34ea9fdd8d0768910f1906
peter enejo <petstorm101@gmail.com>
2022-09-06 16:28:02 +0100

    organizing data into sql columns

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1,"Mr. Chocolate", 20, "M", "brown", "fifteen degrees celcius", 1);
+INSERT INTO bears VALUES (2,"Rowdy", 10, "M", "fair", "twenty degrees celcius", 1);
+INSERT INTO bears VALUES (3,"Tabitha", 6, "F", "white", "thirty degrees celcius", 0);
+INSERT INTO bears VALUES (4,"Sargeant Brown", 19, "M", "purple", "eighty degrees celcius", 1);
+INSERT INTO bears VALUES (5,"Melissa", 13, "F", "dark", "twelve degrees celcius", 1);
+INSERT INTO bears VALUES (6,"Grinch", 2, "M", "Black", "fifty degrees celcius", 1);
+INSERT INTO bears VALUES (7,"Wendy", 12, "F", "Blue", "hundred degrees celcius", 0);
+INSERT INTO bears VALUES (8,null, 20, "M", "black", "thousand degrees celcius", 0);
